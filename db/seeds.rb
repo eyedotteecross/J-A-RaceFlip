0757ff704490b5b166cad7e80883dbc33de04ac9
Jorge Basilio <J.l.Basilio55@gmail.com>
2020-01-22 08:42:35 -0500

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,32 @@
-
-
 acura1 = Car.create(make:"Acura", model:"TL", year:2012, value:28000)
-bmw1 = Car.create(make: "BMW", model: "5 series", year: "2019", value: 40000)
-# ford1 = Car.new("Ford ", "Mustang", "2016", 28000)
+bmw1 = Car.create(make: "BMW", model: "5 series", year: 2019, value: 40000)
+ford1 = Car.create(make:"Ford",model: "Mustang",year: 2016,value: 28000)
+tesla1 = Car.create(make:"Tesla",model: "Model X",year: 2019,value: 100000)
+honda1 = Car.create(make:"Honda",model: "Accord",year: 2020,value: 40000)
+bugatti1 = Car.create(make:"Bugatti",model: "Chiron",year: 2020,value: 2000000)
+tesla2 = Car.create(make:"Tesla",model: "Roadster",year: 2020,value: 180000)
+rolls_royce1 = Car.create(make:"Rolls-Royce",model: "Phantom",year: 2020,value: 1000000)
+lexus1 = Car.create(make:"Lexus",model: "LFA",year: 2020,value: 200000)
+mercedez_benz = Car.create(make:"Mercedes",model: "S-class",year: 2020,value: 138000)
+dodge1 = Car.create(make:"Dodge",model: "Charger",year: 2020,value: 36000)
+bently2 = Car.create(make:"Bently",model: "Bentaya",year: 2020,value: 250000)
+toyota1 = Car.create(make:"Toyota",model: "Camary",year: 2018,value: 29000)
+toyota2 = Car.create(make:"Toyota",model: "carola",year: 2020,value: 25000)
+ford2 = Car.create(make:"Ford",model: "F150",year: 2020,value: 49000)
+cadillac1 = Car.create(make:"Cadillac",model: "Escalade",year: 2018,value: 65000)
+cadillac2 = Car.create(make:"Cadillac",model: "CTS",year: 2018,value: 60000)
+jeep1 = Car.create(make:"Jeep",model: "Wrangler",year: 2020,value: 38000)
+jeep2 = Car.create(make:"Jeep",model: "Cherokee",year: 2020,value: 47000)
+lamborghini1 = Car.create(make:"Lamborghini",model: "Aventador",year: 2020,value: 270000)
+Maybach1 = Car.create(make:"Maybach",model: "S 560",year: 2020,value: 180000)
+chevrolet1 = Car.create(make:"Chevrolet",model: "Corvette",year: 2020,value: 65000)
+ferrari1 = Car.create(make:"Ferrari",model: "458 italia",year: 2020,value: 280000)
+ferrari2 = Car.create(make:"Ferrari",model: "LaFerrari",year: 2020,value: 3500000)
+porche1 = Car.create(make:"Porche",model: "911",year: 2020,value: 230000)
+porche2 = Car.create(make:"Porche",model: "Panamera",year: 2020,value: 280000)
+
+
+
+
+
+
